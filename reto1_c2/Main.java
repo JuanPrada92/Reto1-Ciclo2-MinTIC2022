c743bad2e2ef7f284f7857015b27a9d9703ea0ce
JuanPrada92 <104868748+JuanPrada92@users.noreply.github.com>
2022-07-04 18:38:43 -0500

    Add files via upload

---
+++ reto1_c2/Main.java
@@ -0,0 +1,20 @@
+package reto1_c2;
+
+import Vistas.*;
+import Modelo.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //.Instancia de la vista login
+        Login login = new Login();
+        //El método setVisible hace visible la ventana
+        login.setVisible(true);
+        
+        // crear instancia clase conexion
+        Conexion conexion = new Conexion();
+        conexion.getConnection();
+        
+    }
+
+}
